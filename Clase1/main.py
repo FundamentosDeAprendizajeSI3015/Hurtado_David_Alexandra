abb5d32a45a5628e05e111901a0ecb935272fdd6
Alexandra Hurtado David <141194604+alexahurtado08@users.noreply.github.com>
2026-02-17 12:57:02 -0500

    Ejercicio Iris DataSet

---
+++ Clase1/main.py
@@ -0,0 +1,15 @@
+import numpy
+import pandas
+import sklearn
+import torch
+import tensorflow
+import umap
+
+print("Entorno listo para ML y DL 🚀")
+
+
+import pandas as pd
+
+df = pd.read_csv("iris.csv")
+print(df.head())
+
